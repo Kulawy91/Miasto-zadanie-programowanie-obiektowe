6b50f8596ec25b6ec64ae15b093f1e1ac64335a4
pawel <pawel@KULAWEPC>
2023-05-03 14:22:25 +0100

    Dodaj pliki projektów.

---
+++ Miasto zadanie domowe/Miasto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miasto_zadanie_domowe
+{
+    public class Miasto
+    {
+        private string nazwa;
+
+            public string Nazwa
+            {
+            get { return nazwa; }
+            set { nazwa = value; }
+            }
+
+
+        private uint populacja;
+
+            public uint Populacja
+            {
+            get { return populacja; }
+            set { populacja = value; }
+            }
+
+        private double powierzchnia;
+            public double Powierzchnia
+            {
+            get { return powierzchnia; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Powierzchnia musi być większa od zera.");
+                }
+                powierzchnia = value;
+                }
+            }
+
+        public double GęstośćZaludnienia
+        {
+            get { return Populacja / Powierzchnia; }
+        }
+    }
+}
---
+++ Miasto zadanie domowe/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Miasto_zadanie_domowe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Miasto gdansk = new Miasto();
+            gdansk.Nazwa = "Gdańsk";
+            gdansk.Populacja = 582205;
+            gdansk.Powierzchnia = 262;
+            Console.WriteLine("Gęstość zaludnienia miasta {0} wynosi {1} os/km2", gdansk.Nazwa, gdansk.GęstośćZaludnienia);
+
+            Miasto gdynia = new Miasto();
+            gdynia.Nazwa = "Gdynia";
+            gdynia.Populacja = 244458;
+            gdynia.Powierzchnia = 135.1;
+            Console.WriteLine("Gęstość zaludnienia miasta {0} wynosi {1} os/km2", gdynia.Nazwa, gdynia.GęstośćZaludnienia);
+
+            Miasto sopot = new Miasto();
+            sopot.Nazwa = "Sopot";
+            sopot.Populacja = 37089;
+            sopot.Powierzchnia = 17.31;
+            Console.WriteLine("Gęstość zaludnienia miasta {0} wynosi {1} os/km2", sopot.Nazwa, sopot.GęstośćZaludnienia);
+        }
+    }
+}
